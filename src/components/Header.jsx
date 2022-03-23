c5686ce47d0644cdfea8b297cc130aaa0029325b
Love Efraimsson <loveefraimsson@gmail.com>
2022-03-23 10:32:14 +0100

    Menyn fungerar att fälla ut och in, ingen css än

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,19 +1,34 @@
 import React, { Component } from 'react';
 import '../styles/header.scss';
+import '../styles/mediaQuerie.scss';
 
 import logo from "../images/logo.png";
-
+import hamburgerIcon from "../images/hamburgerIcon.png";
+import crossIcon from "../images/crossIcon.png";
 
 import {
   BrowserRouter as Router,
   Link
 } from "react-router-dom";
 
-export class Header extends Component {
-  
-  
 
+
+export class Header extends Component {
+
+  state = {
+    showMenu: false,
+  }
   
+  handleMenu = () => {
+    if(this.state.showMenu === false) {
+      this.setState({ showMenu: true })
+    }
+    else {
+      this.setState({ showMenu: false })
+    }
+  }
+
+ 
   render() {
 
     let buttons;
@@ -21,19 +36,35 @@ export class Header extends Component {
     //LOGGED IN
     if(localStorage.getItem("loggedIn") === "true") {
       buttons = (
-        <ul className='navbar-nav'>
-          <li className='navItem'>
-            <Link className="navLink" to="/profilepage" >Profilsida</Link>           
-          </li>
+        <section className='toggleContainer'>
 
-           <li className='navItem'>
-            <Link className="navLink" to="/workoutbank" >Övningar</Link>
-          </li>
+          
+            <img src={hamburgerIcon} onClick={this.handleMenu} alt="" />
+            {/* <img src={crossIcon} alt="" /> */}
+          
 
-          <li className='navItem'>
-            <Link className="navLink" to="/" onClick={() => localStorage.clear()}>Logout</Link>
-          </li>
-        </ul>)
+            {this.state.showMenu ? (
+              <ul className='navbar-nav'>
+                <li className='navItem'>
+                  <Link className="navLink" to="/profilepage" >Profilsida</Link>           
+                </li>
+
+                <li className='navItem'>
+                  <Link className="navLink" to="/workoutbank" >Övningar</Link>
+                </li>
+
+                <li className='navItem'>
+                  <Link className="navLink" to="/" onClick={() => localStorage.clear()}>Logout</Link>
+                </li>
+              </ul>
+              ) : null
+
+            }
+          
+          
+        </section>
+        
+      )
     }
 
     //LOGGED OUT
@@ -44,11 +75,9 @@ export class Header extends Component {
     return (
       <header className='header'>
           <img className='logo' src={logo} alt="WorkoutBase-logo" />
-          
+         
           <nav className='navbarContainer'>
-            <section className='navbar'>
               {buttons}
-            </section>
 
           </nav>
           
