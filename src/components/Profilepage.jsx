33edefa7bbfd3fc687a8de8bfbd85a8946626c59
Love Efraimsson <loveefraimsson@gmail.com>
2022-03-30 15:18:12 +0200

    Skapat profilsidan och gjort grund-styling

--- src/components/Profilepage.jsx
+++ src/components/Profilepage.jsx
@@ -1,15 +1,19 @@
 import React, { Component } from 'react';
 import Header from './Header';
+import '../styles/profilePage.scss';
 
 
 export class Profilepage extends Component {
   render() {
     return (
-      <div>
+      <section className='profilePage'>
           <Header />
-          <h1>Profilepage</h1>
+          <h2 className='welcomeTitleProfile'>Hej {localStorage.getItem("userName")}!</h2>
+          <p>Här är din profilsida där du kan komma åt ditt träningsprogram och dina favoritövningar.</p>
           
-      </div>
+          
+
+      </section>
     )
   }
 }
