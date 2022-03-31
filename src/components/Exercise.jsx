17c1166145d4c8138dfba2830127585d9fe563aa
Love Efraimsson <loveefraimsson@gmail.com>
2022-03-31 08:31:04 +0200

    Har lagt in ett formulär för att spara till träningsprogram samt sparat input i state

--- src/components/Exercise.jsx
+++ src/components/Exercise.jsx
@@ -11,9 +11,23 @@ export class Exercise extends Component {
 
     state = {
         exercise: this.props.location.state.exercise,
+        title: this.props.location.state.exercise.title,
         category: this.props.location.state.category,
-        favorite: false,    
+        favorite: false,
+        sets: '',
+        reps: '',
     }
+
+    handleChange = (evt) => {
+      console.log("name", evt.target.name);
+      console.log("value", evt.target.value);
+      //let exerciseTitle = this.state.exercise.title;
+      //let exerciseCategory = this.state.category;
+
+      this.setState({ [evt.target.name]: evt.target.value });
+    }
+
+    
 
     
     favoriteMark = () => {
@@ -32,6 +46,13 @@ export class Exercise extends Component {
       <Header />
         <Link className='backButton' to={"/workoutbank"} >Tillbaka till övningsbanken</Link>
         <h1>{this.state.exercise.title}</h1>
+        
+        <form className='trainingProgramForm'>
+          <p>Vill du spara denna övningen i ditt träningsprogram? Fyll i uppgifter nedan:</p>
+          <input name='sets' type="text" placeholder='Sets' onChange={this.handleChange} /> 
+          <input name='reps' type="text" placeholder='Reps' onChange={this.handleChange} /><br />
+          <button type='submit'>Spara</button>
+        </form>
 
         <button className='favoriteButton' onClick={this.favoriteMark}>Favoritmarkera</button>
 
