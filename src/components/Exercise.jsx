f607630ac009a4811bbd780f7677b24d9785c87f
Love Efraimsson <loveefraimsson@gmail.com>
2022-04-07 09:50:28 +0200

    Error-meddelande eller success-meddelande visas beroende på om övning lagts till i träningsprogrammet

--- src/components/Exercise.jsx
+++ src/components/Exercise.jsx
@@ -10,125 +10,131 @@ const parse = require('html-react-parser');
 
 export class Exercise extends Component {
 
-    state = {
-        exercise: this.props.location.state.exercise,
-        title: this.props.location.state.exercise.title,
-        category: this.props.location.state.category,
-        exerciseArray: this.props.location.state.exerciseArray,
-        favoriteMarked: false,
-        sets: '',
-        reps: '',
-        userName: localStorage.getItem("userName"),
-        favoriteExercises: [],
-        loadedData: false,
+  state = {
+      exercise: this.props.location.state.exercise,
+      title: this.props.location.state.exercise.title,
+      category: this.props.location.state.category,
+      exerciseArray: this.props.location.state.exerciseArray,
+      favoriteMarked: false,
+      sets: null,
+      reps: null,
+      userName: localStorage.getItem("userName"),
+      favoriteExercises: [],
+      loadedData: false,
+      isFilled: '',
+      savedInProgram: false,
+  }
+
+  //Gets all favorite exercises
+  componentDidMount = () => {
+    fetch("http://localhost:3001/favoriteexercises", {
+    method: "post",
+    headers: {
+        "Content-Type": "application/json",
+    },
+    body: JSON.stringify({userName: this.state.userName})
+    })
+    .then(res => res.json())
+    .then(data => {
+        //console.log("data[0].favoriteExercises", data[0].favoriteExercises);
+        this.setState({ loadedData: true })
+
+        let favoriteExercises = data[0].favoriteExercises;
+        let title = this.state.exercise.title;
+        //console.log("title", title);
+
+
+        //Checks if current exercise is marked as favorite, if it is in favorite exercises then sets state to true, otherwise not. Doing this to render the right button
+        favoriteExercises.map((exercise, i) => {
+          if(exercise.title === title) {
+            //console.log("Rätt");
+            this.setState({ favoriteMarked: true })
+          }
+          else {
+            //this.setState({ favoriteMarked: false })
+          }
+        })
+    }); 
+  }
+
+
+  
+  favoriteMarkToggle = () => {
+    let exerciseTitle = this.state.exercise.title;
+    let exerciseCategory = this.state.category;
+
+    //Sets the exercise to save in databse to object
+    let favoriteExercise = {
+      title: exerciseTitle,
+      category: exerciseCategory,
+      description1: this.state.exercise.description1,
+      description2: this.state.exercise.description2,
+      image: this.state.exercise.image,
+      video: this.state.exercise.video,
+      userName: localStorage.getItem("userName")
     }
 
-    //Gets all favorite exercises
-    componentDidMount = () => {
-      fetch("http://localhost:3001/favoriteexercises", {
-      method: "post",
-      headers: {
-          "Content-Type": "application/json",
-      },
-      body: JSON.stringify({userName: this.state.userName})
+    //console.log(favoriteExercise);
+    
+    //Saves exercise as favorite in database
+    if(this.state.favoriteMarked === false) {
+
+      this.setState({ favoriteMarked: true })
+
+      fetch("http://localhost:3001/savefavorite", {
+        method: "post",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(favoriteExercise)
       })
       .then(res => res.json())
       .then(data => {
-          //console.log("data[0].favoriteExercises", data[0].favoriteExercises);
-          this.setState({ loadedData: true })
-
-          let favoriteExercises = data[0].favoriteExercises;
-          let title = this.state.exercise.title;
-          //console.log("title", title);
-
-
-          //Checks if current exercise is marked as favorite, if it is in favorite exercises then sets state to true, otherwise not. Doing this to render the right button
-          favoriteExercises.map((exercise, i) => {
-            if(exercise.title === title) {
-              //console.log("Rätt");
-              this.setState({ favoriteMarked: true })
-            }
-            else {
-              //this.setState({ favoriteMarked: false })
-            }
-          })
-      }); 
+          //console.log("data", data);
+          //this.setState({ favoriteExercises: data })
+      });
     }
 
+    //Removes exercise from favorite in database
+    else {
+      this.setState({ favoriteMarked: false });
 
+      fetch("http://localhost:3001/removeexercise", {
+        method: "post",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(favoriteExercise)
+      })
+      .then(res => res.json())
+      .then(data => {
+          //console.log("data", data);
+      });
+
+    }
+  }
+
+
+
+
+
+
+  handleChange = (evt) => {
+    //console.log("name", evt.target.name);
+    //console.log("value", evt.target.value);
+    //let exerciseTitle = this.state.exercise.title;
+    //let exerciseCategory = this.state.category;
+
+    this.setState({ [evt.target.name]: evt.target.value });
+  }
+
+  saveInProgram = (evt) => {
+    evt.preventDefault();
+
+    // if(this.state.sets != null && this.state.sets != " " && this.state.reps != null && this.state.reps != " ") {
     
-    favoriteMarkToggle = () => {
-      let exerciseTitle = this.state.exercise.title;
-      let exerciseCategory = this.state.category;
-
-      //Sets the exercise to save in databse to object
-      let favoriteExercise = {
-        title: exerciseTitle,
-        category: exerciseCategory,
-        description1: this.state.exercise.description1,
-        description2: this.state.exercise.description2,
-        image: this.state.exercise.image,
-        video: this.state.exercise.video,
-        userName: localStorage.getItem("userName")
-      }
-
-      //console.log(favoriteExercise);
-      
-      //Saves exercise as favorite in database
-      if(this.state.favoriteMarked === false) {
-
-        this.setState({ favoriteMarked: true })
-
-        fetch("http://localhost:3001/savefavorite", {
-          method: "post",
-          headers: {
-              "Content-Type": "application/json",
-          },
-          body: JSON.stringify(favoriteExercise)
-        })
-        .then(res => res.json())
-        .then(data => {
-            //console.log("data", data);
-            //this.setState({ favoriteExercises: data })
-        });
-      }
-
-      //Removes exercise from favorite in database
-      else {
-        this.setState({ favoriteMarked: false });
-
-        fetch("http://localhost:3001/removeexercise", {
-          method: "post",
-          headers: {
-              "Content-Type": "application/json",
-          },
-          body: JSON.stringify(favoriteExercise)
-        })
-        .then(res => res.json())
-        .then(data => {
-            //console.log("data", data);
-        });
-
-      }
-    }
-
-
-
-
-
-
-    handleChange = (evt) => {
-      //console.log("name", evt.target.name);
-      //console.log("value", evt.target.value);
-      //let exerciseTitle = this.state.exercise.title;
-      //let exerciseCategory = this.state.category;
-
-      this.setState({ [evt.target.name]: evt.target.value });
-    }
-
-    saveInProgram = (evt) => {
-      evt.preventDefault();
-
+    if(this.state.sets && this.state.reps) {
+      console.log("Inte null");
       let addThisExercise = {
         title: this.state.title,
         category: this.state.category,
@@ -137,6 +143,7 @@ export class Exercise extends Component {
         userName: this.state.userName
       }
       console.log(addThisExercise);
+      this.setState({ isFilled: true });
 
       fetch("http://localhost:3001/addinprogram", {
           method: "post",
@@ -148,9 +155,33 @@ export class Exercise extends Component {
         .then(res => res.json())
         .then(data => {
             console.log("data", data);
-            
+            this.setState({ savedInProgram: true });
+            document.getElementById("inputSets").value = "";
+            document.getElementById("inputReps").value = "";
         });
+      
     }
+    else {
+      console.log("Något är null, alla fält måste vara ifyllda");
+      this.setState({ isFilled: false })
+    }
+
+    
+  }
+
+  printErrorMessage = () => {
+   
+      if(this.state.isFilled === false) {
+          return <p className='errorMessage'>Båda fälten måste vara ifyllda!</p>;
+      }
+      else if(this.state.isFilled === true) {
+          return <p className='successMessage'>Övningen har lagts till i ditt träningsprogram!</p>;
+      }
+      else {
+        return <p></p>
+      }
+
+  }
 
   render() {
     if(!this.state.loadedData) return <></>
@@ -166,9 +197,10 @@ export class Exercise extends Component {
         
         <form className='trainingProgramForm' onSubmit={this.saveInProgram}>
           <p>Vill du spara denna övningen i ditt träningsprogram? Fyll i uppgifter nedan med siffror:</p>
-          <input name='sets' type="number" placeholder='Sets; ex. 1' onChange={this.handleChange} /> 
-          <input name='reps' type="number" placeholder='Reps; ex. 4' onChange={this.handleChange} /><br />
+          <input id="inputSets" name='sets' type="number" placeholder='Sets; ex. 1' onChange={this.handleChange} /> 
+          <input id="inputReps" name='reps' type="number" placeholder='Reps; ex. 4' onChange={this.handleChange} /><br />
           <button type='submit'>Spara</button>
+          {this.printErrorMessage()}  
         </form>
 
         {this.state.favoriteMarked ? (
