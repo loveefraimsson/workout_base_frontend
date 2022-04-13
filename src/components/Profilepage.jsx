ef906905faa3b6cefc9db54d5a2a6d99fee2085c
Love Efraimsson <loveefraimsson@gmail.com>
2022-04-13 09:42:18 +0200

    Styling mobil profilsida

--- src/components/Profilepage.jsx
+++ src/components/Profilepage.jsx
@@ -8,15 +8,18 @@ import { Link } from 'react-router-dom';
 export class Profilepage extends Component {
   render() {
     return (
-      <section className='profilePage'>
-          <Header />
-          <h2 className='welcomeTitleProfile'>Hej {localStorage.getItem("userName")}!</h2>
-          <p>Här är din profilsida där du kan komma åt ditt träningsprogram och dina favoritövningar.</p>
-          <Link to="trainingprogram">Ditt träningsprogram</Link><br />
-          <Link to="favoriteexercises">Dina favoritövningar</Link>
-          
+      <>
+        <Header />
+        <section className='profilePage'>
+            
+            <h2 className='welcomeTitleProfile'>Hej {localStorage.getItem("userName")}!</h2>
+            <p>Här är din profilsida där du kan komma åt ditt träningsprogram och dina favoritövningar.</p>
+            <Link className='trainingProgramLink' to="trainingprogram">Ditt träningsprogram</Link>
+            <Link className='favoriteExerciseLink' to="favoriteexercises">Dina favoritövningar</Link>
+            
 
-      </section>
+        </section>
+      </>
     )
   }
 }
